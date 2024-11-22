587788ded25f5b484b51737f61501f20c454b875
Jois <putujois0703@gmail.com>
2024-11-22 10:00:47 +0800

    perbaikan ukuran image

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,7 +1,7 @@
 <template>
-    <img class="md:w-48 w-24" src="./icons/logoipsum.png" alt="" />
-    <div class="profile flex items-center gap-4 text-xl mx-5">
-      <img class="w-8 rounded-full" src="./icons/image.jpg" alt="" />
-      <div class="md:text-xl text-sm">Jois Tika</div>
-    </div>
+  <img class="md:w-52 sm:w-48 w-32" src="./icons/logoipsum.png" alt="" />
+  <div class="profile flex items-center gap-4 text-xl mx-5">
+    <img class="w-8 rounded-full" src="./icons/image.jpg" alt="" />
+    <div class="md:text-xl text-sm">Jois Tika</div>
+  </div>
 </template>
